4a5d1932b5a53ea1e0c7238cc678eebd5f1664c4
gomczak <40300690+gomczak@users.noreply.github.com>
2025-01-27 12:48:44 +0100

    Update config.js to eu-north

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1:a71d7496-cf1a-4969-83d5-f78019c1b1d7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2c7ct4727l9db1s4vhpcv48idg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2rikdu0vabvpuui64jef8fccnj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
