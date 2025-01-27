c4fc42b8f3c7716ff9534c2702d43cc8ef6a8348
gomczak <40300690+gomczak@users.noreply.github.com>
2025-01-27 12:54:56 +0100

    Update config.js userPoolId

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1:a71d7496-cf1a-4969-83d5-f78019c1b1d7', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-north-1_wjrpA97R97', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2rikdu0vabvpuui64jef8fccnj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
